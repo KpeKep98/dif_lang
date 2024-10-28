35499ac7a6230624915fff602f1b0bbc24c8eaf3
Markelov Vladislav <Markelov.VI@infotecs-nt>
2024-10-28 19:43:42 +0300

    Добавил фикстуру инициализации браузера, начал писать метод test items

--- conftest.py
+++ conftest.py
@@ -1,1 +1,13 @@
-Фикстуры
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture(scope="function")
+def browser():
+    service = Service(ChromeDriverManager().install())
+    browser = webdriver.Chrome(service=service)
+    yield browser
+    browser.quit()
+
+#@pytest.mark.parametrize('language'):
--- test_items.py
+++ test_items.py
@@ -1,1 +1,16 @@
-Здесь будет код
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+
+def test_language(browser):
+    browser.get('https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/')
+    try:
+        find_button = browser.find_element(By.XPATH, '//*[@id="add_to_basket_form"]/but')
+        assert find_button, 'Элемент не найден'
+    except NoSuchElementException:
+        assert False, 'Элемент не найден 2'
