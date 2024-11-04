e44a17324aa96da26912eaf480ced23511d0b1f7
KpeKep98 <vlad--mark@mail.ru>
2024-11-04 16:43:25 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 
-def test_language(browser):
+def test_language_and_find_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     try:
         find_button = browser.find_element(By.XPATH, '//*[@id="add_to_basket_form"]/button')
