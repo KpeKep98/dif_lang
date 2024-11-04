eab6e6a2fd497c0437a98ca7cef456940013619c
vladislav <vlad--mark@mail.ru>
2024-11-04 15:21:24 +0300

    Закончил написание автотеста

--- conftest.py
+++ conftest.py
@@ -1,13 +1,22 @@
 import pytest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture(scope="function")
-def browser():
+def browser(request):
     service = Service(ChromeDriverManager().install())
-    browser = webdriver.Chrome(service=service)
+    options = Options()
+
+    language = request.config.getoption("--language")
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(service=service, options=options)
     yield browser
     browser.quit()
 
-#@pytest.mark.parametrize('language'):
+
+def pytest_addoption(parser):
+    parser.addoption("--language", default="en")
--- test_items.py
+++ test_items.py
@@ -1,16 +1,11 @@
-import time
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 
 def test_language(browser):
-    browser.get('https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/')
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     try:
-        find_button = browser.find_element(By.XPATH, '//*[@id="add_to_basket_form"]/but')
-        assert find_button, 'Элемент не найден'
+        find_button = browser.find_element(By.XPATH, '//*[@id="add_to_basket_form"]/button')
+        assert find_button
     except NoSuchElementException:
-        assert False, 'Элемент не найден 2'
+        assert False, 'Элемент не найден!'
