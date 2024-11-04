87af0eaeb5276692e2f0d5f24f3ad5ded449ac26
vladislav <vlad--mark@mail.ru>
2024-11-04 15:49:13 +0300

    провел финальные правки и тесты

--- test_items.py
+++ test_items.py
@@ -1,3 +1,4 @@
+import time
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
@@ -6,6 +7,7 @@ def test_language(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     try:
         find_button = browser.find_element(By.XPATH, '//*[@id="add_to_basket_form"]/button')
+        # time.sleep()
         assert find_button
     except NoSuchElementException:
         assert False, 'Элемент не найден!'
